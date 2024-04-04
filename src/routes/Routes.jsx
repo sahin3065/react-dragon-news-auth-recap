40a1907699c8805a3d4e9851494837450d4e9c28
sahin3065 <sahinalom5161@gmail.com>
2024-04-04 23:41:01 +0600

    added login route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
     {
@@ -9,7 +10,11 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <Home></Home>
+                element: <Home></Home>,
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
             }
         ]
     }    
